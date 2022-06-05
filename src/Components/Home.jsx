c42c1a8a925404ec779d8bcceab8bddfed39a3f7
Rita <94860684+ritullia@users.noreply.github.com>
2022-06-05 15:02:50 +0300

    Home page text update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,10 +4,46 @@ function Home() {
             <h1>Everything about healthy food</h1>
             <div className="pText">
                 <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Accusantium necessitatibus repellendus harum, tempore dicta
-                    nemo in odit veniam, vel dignissimos omnis quaerat nulla.
-                    Quam, laboriosam?
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Exercitationem nostrum repellat eveniet obcaecati suscipit
+                    id incidunt, harum animi perspiciatis quidem doloribus
+                    doloremque, ab iure laudantium sint voluptas, accusantium
+                    necessitatibus sapiente natus quae sed odio molestias.
+                    Laudantium nobis totam consectetur! Excepturi at sit quas
+                    eos fugiat laudantium tenetur nesciunt, voluptatem,
+                    reprehenderit fuga reiciendis totam amet, sed delectus
+                    numquam error. Recusandae ducimus cum voluptas aliquid,
+                    rerum minus quo nemo voluptatem eveniet dolore quasi
+                    laudantium blanditiis error repellendus enim velit unde
+                    consequatur tempore aspernatur. Voluptate, iusto est tenetur
+                    et, provident quis laboriosam iste quaerat aperiam ex velit,
+                    earum consectetur. Reprehenderit voluptate impedit facere
+                    sed libero iusto totam veritatis, eveniet delectus placeat.
+                    Tempore eaque voluptate delectus sed ipsum aut dolor
+                    obcaecati, nostrum corrupti dolorum nam cumque magnam
+                    explicabo doloremque, alias quae quibusdam necessitatibus
+                    beatae dolorem iure possimus sit cum iusto modi? Deserunt
+                    architecto qui quod voluptate aliquid. Ipsa iste ullam
+                    repellat voluptatem non totam, veritatis corporis, vero
+                    cupiditate eum labore dolores aut eligendi sit ipsam dolor
+                    aspernatur optio odio atque nobis consequatur nisi soluta
+                    quaerat. Vitae numquam sequi, dolorum culpa eaque commodi
+                    harum molestiae, placeat omnis voluptas a sed provident ut
+                    porro. Sed libero deleniti aspernatur pariatur ex accusamus,
+                    dolore ipsum dicta corporis eaque quidem vel quaerat in
+                    facilis. Inventore voluptatibus aspernatur architecto
+                    fugiat! Veritatis temporibus at ipsum est quam dolores quis
+                    odio. Fugit molestiae vitae sint esse eveniet nihil culpa,
+                    delectus tenetur minus nam facere ipsum aliquam deserunt,
+                    dolores qui. Quos maiores minus quidem rem minima quaerat,
+                    facere ducimus atque quae possimus! Laborum iure ipsa,
+                    officia voluptatibus eaque perspiciatis, dolorum inventore
+                    repellendus est soluta alias cumque! Ducimus quo rem
+                    blanditiis voluptatibus sint repellat cumque odio provident
+                    maiores optio corporis exercitationem inventore dicta dolor
+                    quaerat, consectetur itaque, corrupti neque eius dolores
+                    earum. Commodi atque, corporis doloremque ab consectetur
+                    accusamus blanditiis ex expedita veniam qui!
                 </p>
             </div>
         </div>
